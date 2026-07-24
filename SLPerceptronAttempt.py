95c38dc88af79016c428fe6eb04d3959b2632c5f
Aaron <278556327+Hadron-Mk1@users.noreply.github.com>
2026-07-24 13:45:50 +0100

    SL perceptron code 'frame' has been created

--- SLPerceptronAttempt.py
+++ SLPerceptronAttempt.py
@@ -1,2 +1,30 @@
-#SL perceptron Logic
+#SL perceptron skeleton
 #---------------------
+
+import numpy
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+# P1:
+class SLPerceptronAttempt:
+    def __init__(self):
+        pass
+
+
+    #P3:
+    def fit(self):
+        pass
+
+    #P4:
+    def predict(self):
+        pass
+
+
+
+# P2:
+def step_function():
+    pass
+
+#P5: Generate testing
+if __name__ == "__main__":
+    pass
