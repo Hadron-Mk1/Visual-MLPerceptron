a99953e548a9b960fb73e18fa44d304e1bb4c906
AameliaH <amelia.huang132@gmail.com>
2026-07-28 17:08:38 +0100

    Changed variable names for line points

--- SLPerceptronAttempt.py
+++ SLPerceptronAttempt.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
         accuracy = numpy.sum(y_hat == y)/len(y_hat)
         return accuracy
 
-    X, y = datasets.make_blobs(n_samples=500, n_features=2, centers = 2, cluster_std=1.0, center_box=(-10, 10), random_state=1)
+    X, y = datasets.make_blobs(n_samples=250, n_features= 2, centers = 2, cluster_std= 1.0, center_box = (-10, 10), random_state = 1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 1)
 
     perceptron = SLPerceptronAttempt(learning_rate=None,training_loops=None,weights=None,bias=None)
@@ -80,13 +80,13 @@ if __name__ == "__main__":
     ax = fig.add_subplot(1,1,1)
     plt.scatter(X_train[:, 0], X_train[:, 1], marker="o", c=y_train)
 
-    x0_1 = numpy.amin(X_train[:,0])
-    x0_2 = numpy.amax(X_train[:,0])
+    x1 = numpy.amin(X_train[:,0])
+    x2 = numpy.amax(X_train[:,0])
 
-    x1_1 = (-perceptron.weights[0] * x0_1 - perceptron.bias) / perceptron.weights[1]
-    x1_2 = (-perceptron.weights[0] * x0_2 - perceptron.bias) / perceptron.weights[1]
+    y1 = (-perceptron.weights[0] * x1 - perceptron.bias) / perceptron.weights[1]
+    y2 = (-perceptron.weights[0] * x2 - perceptron.bias) / perceptron.weights[1]
 
-    ax.plot([x0_1,x0_2],[x1_1,x1_2],'r')
+    ax.plot([x1,x2],[y1,y2],'r')
 
     y_min = numpy.amin(X_train[:,1])
     y_max = numpy.amax(X_train[:,1])
