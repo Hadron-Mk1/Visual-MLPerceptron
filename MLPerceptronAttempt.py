fa0b758a61757990c59ffad951f3f21f48846c79
Aaron <278556327+Hadron-Mk1@users.noreply.github.com>
2026-08-04 19:23:24 +0100

    sigmoid and softmax have been created

--- MLPerceptronAttempt.py
+++ MLPerceptronAttempt.py
@@ -5,11 +5,21 @@ class SLPerceptronAttempt:
         pass
 
 
-def sigmoid(z):
-    pass
+#Function to allow all continuous input to be mapped between 0 or 1
+def sigmoid(value):
+    return 1 / (1 + np.exp(-value))
 
-def softmax(z):
-    pass
+
+def softmax(vector):
+    #Preventing exponential modifiers from becoming too large by enuring a range of -x to 0
+    vector_adjusted = vector - np.max(vector)
+    #All vector values are now in their exponential form
+    probability_no_summation_division = np.exp(vector_adjusted)
+    summation_of_exp_vector = sum(probability_no_summation_division)
+    #Dividing each vector value by the sum of all vector values to create a probability distribution
+    probability_vector = probability_no_summation_division / summation_of_exp_vector
+
+    return probability_vector
 
 def forward_propagation(z):
     pass
